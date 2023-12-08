4bc60001566da60b87ac3efec39c03e3590f4b74
Todd Roling <Todd.Roling@gmail.com>
2023-12-08 11:03:59 -0600

    Better variable name

--- BasicInputOutput/BasicInputOutput.cpp
+++ BasicInputOutput/BasicInputOutput.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-	int x{};
+	int num_in{};
 	while (true) { // due to intentionally undefined exit condition (for now)
-		while (std::cout << "Enter a valid number: " && !(std::cin >> x)) {
+		while (std::cout << "Enter a valid number: " && !(std::cin >> num_in)) {
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		}
-		std::cout << "You entered: " << x << '\n';	
+		std::cout << "You entered: " << num_in << '\n';
 	}
 	return 0;
 }
