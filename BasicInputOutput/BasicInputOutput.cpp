a8fa945dd737a58b7e193da6dabeb09c3b823197
Todd Roling <Todd.Roling@gmail.com>
2023-12-08 10:50:08 -0600

    Doing some input validation

--- BasicInputOutput/BasicInputOutput.cpp
+++ BasicInputOutput/BasicInputOutput.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 int main() {
+	int x{};
 	while (true) { // due to intentionally undefined exit condition (for now)
-		std::cout << "Enter a number: ";
-		int x{};
-		std::cin >> x;
-		std::cout << "You entered: " << x << '\n';
+		while (std::cout << "Enter a valid number: " && !(std::cin >> x)) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		std::cout << "You entered: " << x << '\n';	
 	}
 	return 0;
 }
