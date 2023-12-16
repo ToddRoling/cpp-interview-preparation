139d3f58e4cf1227f2aec308bc8b49f00137a9a1
Todd Roling <Todd.Roling@gmail.com>
2023-12-16 14:16:00 -0600

    HackerRank condiitonal if else

---
+++ ConditionalIfElse/ConditionalIfElse.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem?isFullScreen=true
+
+#include <iostream>
+
+int main() {
+	int n{ 0 };
+	std::cin >> n;
+	if (n == 1)
+		std::cout << "one";
+	else if (n == 2)
+		std::cout << "two";
+	else if (n == 3)
+		std::cout << "three";
+	else if (n == 4)
+		std::cout << "four";
+	else if (n == 5)
+		std::cout << "five";
+	else if (n == 6)
+		std::cout << "six";
+	else if (n == 7)
+		std::cout << "seven";
+	else if (n == 8)
+		std::cout << "eight";
+	else if (n == 9)
+		std::cout << "nine";
+	else
+		std::cout << "Greater than 9";
+}
