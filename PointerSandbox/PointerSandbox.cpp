9882dbccec225d2842c4af5f04578c9f8d1fe6e5
Todd Roling <Todd.Roling@gmail.com>
2023-12-19 09:53:02 -0600

    Pointer stuff

--- PointerSandbox/PointerSandbox.cpp
+++ PointerSandbox/PointerSandbox.cpp
@@ -9,6 +9,7 @@ static void update(int* a, int* b) {
 }
 
 int main() {
+	
 	int a{ 0 };
 	int b{ 0 };
 	int* ptr_a = &a;
@@ -17,5 +18,8 @@ int main() {
 	update(ptr_a, ptr_b);
 	std::cout << a << '\n';
 	std::cout << b;
+	
+	//&a = &a + 1;
+	
 	return 0;
 }
